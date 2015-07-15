de54d5fd04062bad5ab0a6529f4c172e9f0d9041
themeweave <themeweave@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-15 15:30:12 +0000

    Fixed autoplay problem.
    git-svn-id: https://plugins.svn.wordpress.org/wp-elastic-slider/tags/1.1.0@1199417 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/title.php
@@ -0,0 +1,2 @@
+<?php $mb->the_field('name'); ?>
+<input type="text" placeholder="<?php _e('Enter a Slide Name', 'themeweave'); ?>" class="eslide-text" name="<?php $mb->the_name(); ?>" value="<?php echo $mb->get_the_value(); ?>" >
